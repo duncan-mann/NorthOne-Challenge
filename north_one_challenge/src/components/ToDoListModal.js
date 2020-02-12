dbc526788665acfd1dc187a7db502140154f4935
Duncan <duncan.s.mann@gmail.com>
2020-02-11 19:00:34 -0500

    Add state to ToDoListModal form for adding a named To Do list

---
+++ north_one_challenge/src/components/ToDoListModal.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+
+export default function ToDoListModal(props) {
+
+    let styles = {
+        formInput: {
+            width: '100%',
+            border: 'none'
+        }
+    };
+
+    return (
+        <div>
+           <Modal
+            show={props.modalShow}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                Name your list!
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <input 
+                placeholder="Enter list name"
+                style={styles.formInput}
+                value={props.listInput}
+                onChange={event => props.onListInput(event.target.value)}
+                ></input>
+            </Modal.Body>
+            <Modal.Footer>
+            <Button
+            >Add</Button>
+                <Button 
+                variant="danger"
+                onClick={props.modalHide}>Close</Button>
+            </Modal.Footer>
+            </Modal>
+        </div>
+  
+    )
+}
